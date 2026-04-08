9aa9b7755e37bfa74d53332d2d4152871376bbb4
a <a>
2026-04-08 16:23:34 +0200

    a

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,12 @@
-const VERSION = "1.0.5";
+const VERSION = "1.0.6";
 const ROOT = "/accioplagues/";
 
-// Activación: limpia caches antiguos
+// Instalación: activar inmediatamente
+self.addEventListener("install", event => {
+	self.skipWaiting();
+});
+
+// Activación: limpiar caches antiguos
 self.addEventListener("activate", event => {
 	event.waitUntil(
 		caches.keys().then(keys =>
@@ -11,29 +16,38 @@ self.addEventListener("activate", event => {
 	self.clients.claim();
 });
 
-// Fetch: cache dinámico + fallback
+// Fetch
 self.addEventListener("fetch", event => {
 	const request = event.request;
 
 	if (request.method !== "GET") return;
 
+	// Network-first para HTML
+	if (request.headers.get("accept").includes("text/html")) {
+		event.respondWith(
+			fetch(request)
+				.then(response => {
+					const cloned = response.clone();
+					caches.open(VERSION).then(cache => cache.put(request, cloned));
+					return response;
+				})
+				.catch(() => caches.match(request))
+		);
+		return;
+	}
+
+	// Cache-first para el resto
 	event.respondWith(
 		caches.match(request).then(cached => {
 			if (cached) return cached;
 
-			return fetch(request)
-				.then(response => {
-					if (response && response.status === 200 && response.type === "basic") {
-						const cloned = response.clone();
-						caches.open(VERSION).then(cache => cache.put(request, cloned));
-					}
-					return response;
-				})
-				// .catch(() => {
-				// 	if (request.headers.get("accept").includes("text/html")) {
-				// 		return caches.match(`${ROOT}index.html`);
-				// 	}
-				// });
+			return fetch(request).then(response => {
+				if (response && response.status === 200 && response.type === "basic") {
+					const cloned = response.clone();
+					caches.open(VERSION).then(cache => cache.put(request, cloned));
+				}
+				return response;
+			});
 		})
 	);
 });
