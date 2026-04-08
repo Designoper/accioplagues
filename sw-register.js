f5f26ccdea5d1e9dcacd759406cb753c5e68619d
a <a>
2026-04-08 21:28:16 +0200

    a

--- sw-register.js
+++ sw-register.js
@@ -1,7 +1,7 @@
 const ROOT = "/accioplagues/";
 
 if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register(`${ROOT}service-worker.js`)
+	navigator.serviceWorker.register(`${ROOT}sw.js`)
 		.then(() => console.log("Service Worker registrado"))
 		.catch(err => console.error("Error al registrar SW:", err));
 }
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1.0.15";
+const VERSION = "1.0.16";
 
 // Instalación: activar inmediatamente
 self.addEventListener("install", event => {
