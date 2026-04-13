78351a16a6a92721580b2a3cbc0ef027e0c1e3b3
a <a>
2026-04-13 12:00:13 +0200

    a

--- sw-register.js
+++ sw-register.js
@@ -1,7 +1,7 @@
 const ROOT = "/accioplagues/";
 
-if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register(`${ROOT}sw.js`)
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register(`${ROOT}sw.js?v=${Date.now()}`)
 		.then(() => console.log("Service Worker registrado"))
 		.catch(err => console.error("Error al registrar SW:", err));
 }
