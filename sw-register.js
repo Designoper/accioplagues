d908081c00cdb6000527d7237f5eb2f118bc21db
a <a>
2026-04-13 12:36:50 +0200

    a

--- sw-register.js
+++ sw-register.js
@@ -1,22 +1,7 @@
 const ROOT = "/accioplagues/";
 
-// if ('serviceWorker' in navigator) {
-// 	navigator.serviceWorker.register(`${ROOT}sw.js?v=${Date.now()}`)
-// 		.then(() => console.log("Service Worker registrado"))
-// 		.catch(err => console.error("Error al registrar SW:", err));
-// }
-
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register(`${ROOT}sw.js?v=${Date.now()}`)
-		.then(reg => {
-			reg.addEventListener('updatefound', () => {
-				const newSW = reg.installing;
-				newSW.addEventListener('statechange', () => {
-					if (newSW.state === 'installed' && navigator.serviceWorker.controller) {
-						// Recargar para activar el SW nuevo
-						location.reload();
-					}
-				});
-			});
-		});
+		.then(() => console.log("Service Worker registrado"))
+		.catch(err => console.error("Error al registrar SW:", err));
 }
