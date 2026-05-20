41a92e2d36149784515e8c36a54198ca848c8d98
a <a>
2026-05-20 22:35:31 +0200

    a

--- sw-register.js
+++ sw-register.js
@@ -1,13 +1,24 @@
+// const PATHNAME = "/accioplagues/";
+// let queryString;
+// const FORCE_NEW_SW = true;
+
+// FORCE_NEW_SW
+// 	? queryString = `?v=${Date.now()}`
+// 	: queryString = "";
+
+// if ('serviceWorker' in navigator) {
+// 	navigator.serviceWorker.register(`${PATHNAME}sw.js${queryString}`)
+// 		.then(() => console.log("Service Worker registrado"))
+// 		.catch(err => console.error("Error al registrar SW:", err));
+// }
+
 const PATHNAME = "/accioplagues/";
-let queryString;
-const FORCE_NEW_SW = true;
+const FILENAME = "sw.js";
 
-FORCE_NEW_SW
-	? queryString = `?v=${Date.now()}`
-	: queryString = "";
-
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register(`${PATHNAME}sw.js${queryString}`)
-		.then(() => console.log("Service Worker registrado"))
-		.catch(err => console.error("Error al registrar SW:", err));
+if (navigator.serviceWorker) {
+	try {
+		await navigator.serviceWorker.register(`${PATHNAME}${FILENAME}`, { type: 'module' });
+	} catch (err) {
+		console.error("Error al registrar SW:", err);
+	}
 }
